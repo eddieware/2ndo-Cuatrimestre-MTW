eafb497dcb479b51633d9b4b51ff82b2401c5f40
EDUARDO <ing.eduardo.mejia.bautista@gmail.com>
2019-06-16 21:13:45 -0500

    PRACTICE9_COMPLETED

---
+++ Xamarin.Android/PRACTICA9/VistaPersonalizada2/VistaPersonalizada2/ColorButton.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace VistaPersonalizada2.Views
+{
+ 
+    class ColorButton : Button 
+    {
+        public int valor { get; set; }
+        public string colorInicial { get; set; }
+        public string colorProcesando { get; set; }
+
+        public ColorButton(Context context) : base(context)
+        {
+            colorInicial = "#ffbf360c";
+            colorProcesando = "#11bf360c";
+            valor = 100000000;
+            Click += OnClicked; // mAqui se agrego el metodo onclicked para que al tocar se dispare
+        }
+        public ColorButton(Context context, Android.Util.IAttributeSet attrs) : base(context, attrs)
+        {
+            colorInicial = "#ffbf360c";
+            colorProcesando = "#11bf360c";
+            valor = 100000000;
+            Click += OnClicked;
+        }
+
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            this.SetBackgroundColor(Android.Graphics.Color.ParseColor(colorProcesando));
+            await System.Threading.Tasks.Task.Run(Tarea); this.SetBackgroundColor(Android.Graphics.Color.ParseColor(colorInicial));
+        }
+
+        private void Tarea()
+        {
+            int i = 0;
+            while (i < valor)
+                i++;
+        }
+
+
+        public override IParcelable OnSaveInstanceState()
+        {
+            IParcelable state = base.OnSaveInstanceState();
+            ViewState instance = new ViewState(state)
+            {
+                valor = valor,
+                colorInicial = colorInicial,
+                colorProcesando = colorProcesando
+            };
+            return instance;
+        }
+
+
+        public override void OnRestoreInstanceState(IParcelable state)
+        {
+
+            ViewState instance = state as ViewState;
+            if (instance == null)
+            {
+                base.OnRestoreInstanceState(state);
+            }
+            else
+            {
+                base.OnRestoreInstanceState(instance.SuperState);
+                valor = instance.valor;
+                colorInicial = instance.colorInicial;
+                colorProcesando = instance.colorProcesando;
+            }
+        }
+
+
+    }
+}
---
+++ Xamarin.Android/PRACTICA9/VistaPersonalizada2/VistaPersonalizada2/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using VistaPersonalizada2.Views;
+
+
+
+namespace VistaPersonalizada2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            if (savedInstanceState != null)
+                SupportActionBar.Title = savedInstanceState.GetString("LLAVE_GUARDADA");
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            RelativeLayout relativeLayout = FindViewById<RelativeLayout>(Resource.Id.relativeLayout);
+            ColorButton myColorButton = new ColorButton(this);
+            myColorButton.Text = "My Color Button";
+
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent);
+            layoutParams.AddRule(LayoutRules.RightOf, Resource.Id.colorbutton1);
+            myColorButton.LayoutParameters = layoutParams;
+            relativeLayout.AddView(myColorButton);
+
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutString("LLAVE_GUARDADA", "Llave guardada");
+        }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+            string value = savedInstanceState.GetString("LLAVE_GUARDADA");
+        }// Ultimo codigo agregado
+
+
+
+    }
+}
