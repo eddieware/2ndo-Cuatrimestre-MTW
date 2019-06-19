6db5942746dd465d3d56d07611c9637850cd431d
EDUARDO <ing.eduardo.mejia.bautista@gmail.com>
2019-06-19 09:31:47 -0500

    clickCounter

---
+++ Xamarin.Android/TUTORIALS_DONE/Practice1ClickEvent/Practice1ClickEvent/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Practice1ClickEvent
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 1;
+        protected override void OnCreate(Bundle savedInstanceState)// ON CREATHE METHOD
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.eddiebutton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+        }
+    }
+}
